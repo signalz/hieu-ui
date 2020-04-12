bcc7c9c8664851edfbed35c274616c69a7977e70
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-04-12 22:47:44 +0700

    duration time

--- src/App.js
+++ src/App.js
@@ -14,20 +14,26 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: [],
+      requestDuration: 0,
     };
   }
 
   fetchData = (searchObj) => {
     const { accNo, startDate, endDate } = searchObj;
-    console.log(moment().isValid(startDate));
     const queryObj = {
       accNo: accNo || null,
       from: startDate ? moment(startDate).format("YYYY-MM-DD") : null,
       to: endDate ? moment(endDate).format("YYYY-MM-DD") : null,
     };
+    const startRequestTime = moment();
     message.loading("Loading data ...", 1);
     fetch(`${ENDPOINT}?${qs.stringify(queryObj)}`)
-      .then((res) => (res.ok ? res.json() : Promise.reject("Something wrong")))
+      .then((res) => {
+        this.setState({
+          requestDuration: moment() - startRequestTime,
+        });
+        return res.ok ? res.json() : Promise.reject("Something wrong");
+      })
       .then((data) => {
         this.setState({ data });
         message.success("Load data successfully");
@@ -70,6 +76,10 @@ class App extends React.Component {
             ),
           }}
         />
+        <div>
+          <span>Request duration time: </span>
+          <span>{this.state.requestDuration} ms</span>
+        </div>
       </div>
     );
   }
